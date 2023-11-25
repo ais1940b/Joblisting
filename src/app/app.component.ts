4a2eee7ed66466fbd3cddde137d25184c5f083b5
ais1940b <itsamitagarwal.agarwal@gmail.com>
2023-11-25 21:34:21 +0530

    Commit Code.
    Signed-off-by: ais1940b <itsamitagarwal.agarwal@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular 13';
+  title = 'Job List';
+  CompanyJobList: any;
+  allData: any;
+  jobRoleList: any;
+  technologyList: any;
+  experienceList: any;
+  selectedJobRoles: any[];
+  selectedTechnologys: any[];
+  selectedExperience: any[];
+  constructor(private http: HttpClient) {
+    this.getUser().subscribe((data: any) => {
+      console.log('data',data);
+      this.allData =  JSON.parse(JSON.stringify(data));
+      this.CompanyJobList = data;
+      this.jobRoleList = data.role;
+      this.technologyList = data.technology;
+      this.experienceList = data.experience;
+    })
+  }
+
+  onJobChange(selectedJobRoles: any){
+    console.log('selectedJobRoles',selectedJobRoles);
+    var filterValue = this.allData.data.filter((value: any) => 
+    { 
+      return this.selectedJobRoles.includes(value.role); 
+    
+    }
+    
+    )
+    this.CompanyJobList.data = filterValue;
+  }
+
+  onTechChange(selectedTechnologys: any){
+    console.log('selectedJobRoles',selectedTechnologys);
+    var filterValue = this.allData.data.filter((value: any) => 
+    { 
+      return this.selectedTechnologys.some(arr => {
+        return value.technology.includes(arr);
+      });
+    
+    }
+    
+    )
+    this.CompanyJobList.data = filterValue;
+  }
+
+  onExpChange(selectedExperience: any){
+    console.log('selectedJobRoles',selectedExperience);
+    var filterValue = this.allData.data.filter((value: any) => 
+    { 
+      return this.selectedExperience.includes(value.experience); 
+    
+    }
+    
+    )
+    this.CompanyJobList.data = filterValue;
+  }
+
+  ngAfterViewInit() {}
+
+  ngOnInit(): void {}
+
+  getUser(){
+    return this.http.get('https://codejudge-question-artifacts-dev.s3.amazonaws.com/q-1710/data.json');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from  '@angular/common/http';
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    CommonModule,
+    MatButtonModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
